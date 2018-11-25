1dbcd7acb189cefb1b30f4eabefeda0bc48c6a7f
Robert Isaev <robert.isaev@gmail.com>
2018-11-25 19:27:51 +0200

    Test cases for gHappy exercise

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,51 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    private GHappy gHappy;
+
+    @BeforeEach
+    public void initialize() {
+        this.gHappy = new GHappy();
+    }
+
+    @Test
+    public void doubleGWithinTheCenter() {
+        Assertions.assertTrue(gHappy.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void doubleGAndSingleG() {
+        Assertions.assertFalse(gHappy.gHappy("xxggyygxx"));
+    }
+
+    @Test
+    public void singleGAndDoubleG() {
+        Assertions.assertFalse(gHappy.gHappy("xxgyyggxx"));
+    }
+
+
+    @Test
+    public void singleGInTheCenter() {
+        Assertions.assertFalse(gHappy.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void singleG() {
+        Assertions.assertFalse(gHappy.gHappy("g"));
+    }
+
+    @Test
+    public void doubleG() {
+        Assertions.assertTrue(gHappy.gHappy("gg"));
+    }
+
+    @Test
+    public void tripleG() {
+        Assertions.assertTrue(gHappy.gHappy("ggg"));
+    }
 }
