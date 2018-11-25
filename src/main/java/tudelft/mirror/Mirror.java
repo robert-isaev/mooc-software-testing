235ad279fef5de41383b264385d988f3cd0ffd3d
Robert Isaev <robert.isaev@gmail.com>
2018-11-25 19:12:24 +0200

    To find a bug in mirror class

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -7,15 +7,16 @@ public class Mirror {
 
         int begin = 0;
         int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
+        for (; begin < end ; begin++, end--) {
+
             if (string.charAt(begin) == string.charAt(end)) {
                 mirror += String.valueOf(string.charAt(end));
-            }
-            else {
+
+            } else {
                 break;
             }
         }
 
-        return begin == end ? string : mirror;
+        return mirror;
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,25 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void firstCase() {
+        Mirror mr = new Mirror();
+        Assertions.assertEquals("ab", mr.mirrorEnds("abXYZba"));
+    }
+
+    @Test
+    public void secondCase() {
+        Mirror mr = new Mirror();
+        Assertions.assertEquals("a", mr.mirrorEnds("abca"));
+    }
+
+    @Test
+    public void thirdCase() {
+        Mirror mr = new Mirror();
+        Assertions.assertEquals("a", mr.mirrorEnds("aba"));
+    }
 }
